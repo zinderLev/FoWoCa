39a8261af81e92320d81b4e6a9c0ddbfce671606
Lev-vz <61240519+Lev-vz@users.noreply.github.com>
2025-07-28 13:39:52 +0200

    Cange project struct

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import Fastify from 'fastify';
 import path from 'path';
 import fastifyStatic from '@fastify/static';
-import { ensureUsersDirectoryExists, userFileExists, createUserFile, UserData } from './tools'; // Импортируем функции и интерфейс
+import { ensureUsersDirectoryExists, userFileExists, createUserFile, UserData } from './scripts/tools'; // Импортируем функции и интерфейс
 
 const fastify = Fastify({
   logger: true // Включаем логирование
@@ -11,14 +11,6 @@ const fastify = Fastify({
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, '..', 'public'), // public/ находится на один уровень выше dist/
   prefix: '/', // Файлы из public/ будут доступны по корневому URL
-});
-
-// 2. Регистрация плагина для отдачи скомпилированных клиентских скриптов из 'dist'
-// clientTools.ts компилируется в dist/clientTools.js
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, '..', 'dist'), // dist/ находится на один уровень выше dist/server.js
-  prefix: '/', // clientTools.js будет доступен по /clientTools.js
-  decorateReply: false // Только один fastifyStatic должен декорировать reply
 });
 
 // Интерфейс для тела POST-запроса регистрации
